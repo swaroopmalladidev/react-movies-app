505853bc120f4b8bb7e025f5235e69c43db5fa73
swaroopmalladidev <swaroop.malladi@gmail.com>
2020-02-22 18:39:50 +0530

    Styled login form and added Login button inside it

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -24,7 +24,7 @@ const customStyles = {
 
 const TabContainer = function(props){
     return (
-        <Typography component="div" style={{padding: 0}}>
+        <Typography component="div" style={{padding: 0, textAlign: 'center'}}>
             {props.children}
         </Typography>
     );
@@ -68,7 +68,7 @@ class Header extends Component{
                     contentLabel="Login" 
                     onRequestClose={this.closeModalHandler}
                     style={customStyles}>
-                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                    <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login"/>
                         <Tab label="Register"/>
                     </Tabs>
@@ -77,10 +77,15 @@ class Header extends Component{
                             <InputLabel htmlFor="username">Username</InputLabel>
                             <Input id="username" type="text"/>
                         </FormControl>
+                        <br/>
+                        <br/>
                         <FormControl required>
                             <InputLabel htmlFor="password">Password</InputLabel>
                             <Input id="password" type="password"/>
                         </FormControl>
+                        <br/>
+                        <br/>
+                        <Button variant="contained" color="primary">LOGIN</Button>
                     </TabContainer>
                 </Modal>
             </div>
