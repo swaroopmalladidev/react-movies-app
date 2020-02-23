b827175cba964e9b79441428ec06d7a33e778902
swaroopmalladidev <swaroop.malladi@gmail.com>
2020-02-23 10:34:15 +0530

    Added Apply Button in the card

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -19,6 +19,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
     root: {
@@ -169,6 +170,14 @@ class Home extends Component {
                                         InputLabelProps={{ shrink: true }}
                                     />
                                 </FormControl>
+                                <br /><br />
+                                <FormControl className={classes.formControl}>
+                                    <Button variant="contained" color="primary">
+                                        APPLY
+                                    </Button>
+                                </FormControl>
+                                
+                                
                             </CardContent>
                         </Card>
                     </div>
