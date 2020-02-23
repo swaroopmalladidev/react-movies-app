c2ca0050bcecc7ac0ef342c9e49b10d446c87ae5
swaroopmalladidev <swaroop.malladi@gmail.com>
2020-02-23 13:50:04 +0530

    Added 'BookShow' component and Book Show button on header on details page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
@@ -12,6 +13,7 @@ import Input from '@material-ui/core/Input';
 //import { getThemeProps } from '@material-ui/styles';
 import PropTypes from 'prop-types';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import BookShow from '../../screens/bookshow/BookShow';
 
 const customStyles = {
     content: {
@@ -119,6 +121,10 @@ class Header extends Component{
         this.setState({ contact: e.target.value });
     }
 
+    bookShowHandler = (e) => {
+        ReactDOM.render(<BookShow />, document.getElementById('root'));
+    }
+
     render(){
         return(
             <div>
@@ -129,6 +135,13 @@ class Header extends Component{
                         Login
                     </Button>
                     </div>
+                    {this.props.showBookShowButton === "true" ?
+                        <div className="bookshow-button">
+                            <Button variant="contained" color="primary" onClick={this.bookShowHandler}>
+                                Book Show
+                            </Button>
+                        </div>
+                        : ""}
                 </header>
                 <Modal
                     ariaHideApp={false} 
