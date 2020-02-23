2f55f5cb51057c075fc6c7ed202b73f2c31a4854
swaroopmalladidev <swaroop.malladi@gmail.com>
2020-02-23 06:55:01 +0530

    implemented Password field validation

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -44,12 +44,20 @@ class Header extends Component{
             modalIsOpen: false,
             value: 0,
             usernameRequired: "dispNone",
-            username: ""
+            username: "",
+            passwordRequired: "dispNone",
+            password: ""
         }
     }
 
     openModalHandler = () => {
-        this.setState({ modalIsOpen: true });
+        this.setState({ modalIsOpen: true,
+            value: 0,
+            usernameRequired: "dispNone",
+            username: "",
+            passwordRequired: "dispNone",
+            password: "" 
+        });
     }
 
     closeModalHandler = () => {
@@ -62,10 +70,15 @@ class Header extends Component{
 
     loginClickHandler = () => {
         this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+        this.state.password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
     }
 
     inputUsernameChangeHandler = (e) => {
         this.setState({ username: e.target.value });
+    }
+
+    inputPasswordChangeHandler = (e) => {
+        this.setState({ password: e.target.value });
     }
 
     render(){
@@ -103,7 +116,10 @@ class Header extends Component{
                         <br/>
                         <FormControl required>
                             <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input id="password" type="password"/>
+                            <Input id="password" type="password" password={this.state.password} onChange={this.inputPasswordChangeHandler} />
+                                <FormHelperText className={this.state.passwordRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
                         </FormControl>
                         <br/>
                         <br/>
