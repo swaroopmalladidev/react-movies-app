748347fd42babef0c0f0680ddbbca1e47f16f2a6
swaroopmalladidev <swaroop.malladi@gmail.com>
2020-02-23 12:45:24 +0530

    Added Release Date, Duration, Rating, Plot details

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
+import Typography from '@material-ui/core/Typography';
+import './Details.css';
 
 class Details extends Component {
     constructor() {
@@ -10,7 +12,7 @@ class Details extends Component {
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((mov) => {
             return mov.id === this.props.movieId
@@ -20,15 +22,36 @@ class Details extends Component {
     }
 
     render() {
+        let movie = this.state.movie;
         return (
             <div className="details">
                 <Header />
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
-
+                        <img src={movie.poster_url} alt={movie.title} />
                     </div>
                     <div className="middleDetails">
-
+                        <div>
+                            <Typography variant="headline" component="h2">{movie.title} </Typography>
+                        </div>
+                        <br/>
+                        <div>
+                            <Typography>
+                                <span className="bold">Genres: </span> {movie.genres.join(', ')}
+                            </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold">Duration:</span> {movie.duration} </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold">Release Date:</span> {new Date(movie.release_date).toDateString()} </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold"> Rating:</span> {movie.critics_rating}  </Typography>
+                        </div>
+                        <div className="marginTop16">
+                            <Typography><span className="bold">Plot:</span> <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline} </Typography>
+                        </div>
                     </div>
                     <div className="rightDetails">
 
